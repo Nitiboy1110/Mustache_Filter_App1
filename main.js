b12a834c2527ba2f1fdddfc8b41fa7f0b493f9ce
NITYAM <83535754+Nitiboy1110@users.noreply.github.com>
2021-12-30 17:10:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(300,240);
+    canvas.center();
+}
+
+function draw(){
+}
+
+function take_snapshot(){
+    save('Tera_Clown_Nose.png');
+    
+}
